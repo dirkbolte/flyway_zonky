fa099a28fd1b6b074fa2e5ef7bf1d0d660c071f2
Dirk Bolte <dirk.bolte@gmx.de>
2020-12-07 21:11:13 +0100

    initializr output

---
+++ src/main/kotlin/com/example/flyway_zonky/FlywayZonkyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.flyway_zonky
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlywayZonkyApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlywayZonkyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/flyway_zonky/FlywayZonkyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.flyway_zonky
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlywayZonkyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
